4daa83a53725eae784e8d17ebf5c8a94122ae734
HALUK\thomas.corrie <thomas_corrie@hotmail.com>
2018-09-13 18:00:27 +0100

    Initial incorporation of ContainerPacking

---
+++ Miscellany/ContainerPacking/Entities.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CromulentBisgetti.ContainerPacking;
+using CromulentBisgetti.ContainerPacking.Algorithms;
+using CromulentBisgetti.ContainerPacking.Entities;
+
+namespace Miscellany.ContainerPacking
+{
+    /// <summary>
+    /// Entities
+    /// </summary>
+    public static class Entities
+    {
+        /// <summary>
+        /// Creates a ContainerPacking Container
+        /// </summary>
+        /// <param name="id">id</param>
+        /// <param name="length">length</param>
+        /// <param name="width">width</param>
+        /// <param name="height">height</param>
+        /// <returns name="container">Container object</returns>
+        /// <search>
+        /// containerpacking
+        /// </search>
+        public static Container Container(int id, double length, double width, double height)
+        {
+            decimal dLength = Miscellany.Math.Functions.ToDecimal(length);
+            decimal dWidth = Miscellany.Math.Functions.ToDecimal(width);
+            decimal dHeight = Miscellany.Math.Functions.ToDecimal(height);
+            return new Container(id, dLength, dWidth, dHeight);
+        }
+
+        /// <summary>
+        /// Creates a ContainerPacking Item
+        /// </summary>
+        /// <param name="id">id</param>
+        /// <param name="dim1">dim1</param>
+        /// <param name="dim2">dim2</param>
+        /// <param name="dim3">dim3</param>
+        /// <param name="quantity">quantity</param>
+        /// <returns name="container">Item object</returns>
+        /// <search>
+        /// containerpacking
+        /// </search>
+        public static Item Item(int id, double dim1, double dim2, double dim3, int quantity)
+        {
+            decimal ddim1 = Miscellany.Math.Functions.ToDecimal(dim1);
+            decimal ddim2 = Miscellany.Math.Functions.ToDecimal(dim2);
+            decimal ddim3 = Miscellany.Math.Functions.ToDecimal(dim3);
+            return new Item(id, ddim1, ddim2, ddim3, quantity);
+        }
+    }
+}
---
+++ Miscellany/ContainerPacking/PackingService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CromulentBisgetti.ContainerPacking;
+using CromulentBisgetti.ContainerPacking.Algorithms;
+using CromulentBisgetti.ContainerPacking.Entities;
+using Autodesk.DesignScript.Runtime;
+
+namespace Miscellany.ContainerPacking
+{
+
+    /// <summary>
+    /// PackingService
+    /// </summary>
+    public static class PackingService
+    {
+        /// <summary>
+        /// Runs a packing algorithm given a container and a list of items to pack
+        /// </summary>
+        /// <param name="container">Container</param>
+        /// <param name="itemsToPack">Items to pack</param>
+        /// <returns name="packedItems">Items that were successfully packed</returns>
+        /// <returns name="unpackedItems">Items that were not packed</returns>
+        /// <search>
+        /// containerpacking
+        /// </search>
+        [MultiReturn(new[] { "packedItems", "unpackedItems"})]
+        public static Dictionary<string, ContainerPackingResult> Packs(Container container, List<Item> itemsToPack)
+        {
+            List<Container> containers = new List<Container> { container };
+            List<int> algorithms = new List<int> { 1 };
+            ContainerPackingResult containerPackingResult = CromulentBisgetti.ContainerPacking.PackingService.Pack(containers, itemsToPack, algorithms).FirstOrDefault();
+            AlgorithmPackingResult algorithmPackingResult = containerPackingResult.AlgorithmPackingResults.FirstOrDefault();
+
+            //Return values
+            var d = new Dictionary<string, ContainerPackingResult>();
+            //d.Add("packedItems", display);
+            //d.Add("unpackedItems", pt);
+            return d;
+        }
+    }
+}
